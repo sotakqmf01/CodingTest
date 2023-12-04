b4bd1823d8642b542d09a56c804b9f17a853d55b
JaeHoon <sotakqmf01@naver.com>
2023-12-05 00:06:16 +0900

    test commit

--- 코테/연습.c
+++ 코테/연습.c
@@ -2,23 +2,6 @@
 #include <math.h>
 
 int main(void) {
-	int n = 0, flag = 0;
-	int prime = 0;
-
-	scanf_s("%d", &n);
-
-	for (int i = 2; i <= n; i++) {
-		flag = 0;
-		for (int j = 2; j <= sqrt(i); j++) {
-			if (i % j == 0) {
-				flag = 1;
-				break;
-			}
-		}
-		if (flag == 0)
-			prime++;
-	}
-
-	printf("%d", prime);
+	printf("git commit test\n");
 	return 0;
 }
